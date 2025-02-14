a1b4442d98d4d6d3d82adf0a761dc1e20dbb611e
Rathindra Nath Biswas <rathininfo7@gmail.com>
2025-02-14 18:19:37 +0600

    completed error page

--- src/ErrorPage/ErrorPage.jsx
+++ src/ErrorPage/ErrorPage.jsx
@@ -5,7 +5,7 @@ const ErrorPage = () => {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 text-gray-900">
     <h1 className="text-9xl font-bold">404</h1>
-    <p className="text-xl mt-4">Page not found</p>
+    <p className="text-xl mt-4">Page not found Here</p>
     <Link
       to="/"
       className="mt-6 px-6 py-3 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition"
